b1073026cf7691a2c04043cfea32ea0adcd075ca
fbalazs930 <fbalazs930@gmail.com>
2021-05-12 17:46:18 +0200

    portfolio

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,10 +1,60 @@
 import React from 'react';
+import portfSrc from './Images/portf.jpg'
 
 export default function Projects() {
    
     return (
         <div className='projects'>
-            Projects
+            <div className='projects-container'>
+                <div className='project'>
+                    <div className='p-img'>
+                        <img src={portfSrc} alt=""/>
+                    </div>
+                    <div className='textarea'>
+                        <h2 className='title'>Saját portfólióm</h2>
+                        <p className='text'>
+                            A portfóliómat a saját munkásságom bemutatása érdekében hoztam létre.
+                        </p>
+                        <div className='textarea-icons'>
+                            <i className="fab fa-codepen"></i>
+                            <i className="fab fa-github"></i>
+                            <i className="fas fa-external-link-alt"></i>
+                        </div>
+                    </div>
+                </div>
+                <div className='project'>
+                    <div className='p-img'>
+                        <img src={portfSrc} alt=""/>
+                    </div>
+                    <div className='textarea'>
+                        <h2 className='title'>Saját portfólióm</h2>
+                        <p className='text'>
+                            A portfóliómat a saját munkásságom bemutatása érdekében hoztam létre.
+                        </p>
+                        <div className='textarea-icons'>
+                            <i className="fab fa-codepen"></i>
+                            <i className="fab fa-github"></i>
+                            <i className="fas fa-external-link-alt"></i>
+                        </div>
+                    </div>
+                </div>
+                <div className='project'>
+                    <div className='p-img'>
+                        <img src={portfSrc} alt=""/>
+                    </div>
+                    <div className='textarea'>
+                        <h2 className='title'>Saját portfólióm</h2>
+                        <p className='text'>
+                            A portfóliómat a saját munkásságom bemutatása érdekében hoztam létre.
+                        </p>
+                        <div className='textarea-icons'>
+                            <i className="fab fa-codepen"></i>
+                            <i className="fab fa-github"></i>
+                            <i className="fas fa-external-link-alt"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
