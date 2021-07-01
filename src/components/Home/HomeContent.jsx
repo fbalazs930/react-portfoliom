01ea6107751cb9b052d38f055394a8a7717b011b
fbalazs930 <78440682+fbalazs930@users.noreply.github.com>
2021-07-01 06:42:06 +0200

    Update HomeContent.jsx

--- src/components/Home/HomeContent.jsx
+++ src/components/Home/HomeContent.jsx
@@ -4,7 +4,7 @@ const HomeContent = () => {
     return (
         <div className='home-content'>
             <h1>Farkas Balázs</h1>
-            <h3>Junior Webfejlesztő</h3>
+            <h3>Front-end Webfejlesztő</h3>
         </div>
     )
 }
